fd7c80eb80a229c03483a9089dac2748e0cabbaa
Luke Pryor <lukosan@gmail.com>
2015-12-01 16:53:48 +0000

    Ecommerce bits and bobs

--- src/main/java/org/lukosan/salix/thymeleaf/SalixProcessor.java
+++ src/main/java/org/lukosan/salix/thymeleaf/SalixProcessor.java
@@ -1,6 +1,7 @@
 package org.lukosan.salix.thymeleaf;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.util.Map;
 
@@ -10,6 +11,8 @@ import org.springframework.util.StringUtils;
 
 public class SalixProcessor extends StandardEvaluationContext {
 
+	private static BigDecimal ONEHUNDRED = new BigDecimal(100);
+	
 	public String urlEncode(String href) {
 		try {
 			return URLEncoder.encode(href, "utf8");
@@ -29,6 +32,10 @@ public class SalixProcessor extends StandardEvaluationContext {
 	public String safe(Map<String, Object> map, String expression, String defaultValue) {
 		String value = null == map ? "" : MapUtils.getString(map, expression);
 		return StringUtils.hasText(value) ? value : defaultValue;
+	}
+	
+	public String price(int pence) {
+		return "£" + new BigDecimal(pence).setScale(2).divide(ONEHUNDRED).toPlainString().replaceAll(".00", "");
 	}
 
 }
